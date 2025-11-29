46e318c42d782a5ea2e7b890389bf53eb583620d
Seamus Mullan <43112447+SeamusMullan@users.noreply.github.com>
2025-11-29 21:31:06 +0000

    update build

--- build.sh
+++ build.sh
@@ -3,8 +3,37 @@
 
 set -e
 
-BUILD_TYPE="${1:-Release}"
-USE_SHADERS="${2:-OFF}"
+# Default values
+BUILD_TYPE="Debug"
+USE_SHADERS="OFF"
+
+# Parse command line arguments
+while getopts "rsh" opt; do
+    case $opt in
+        r)
+            BUILD_TYPE="Release"
+            ;;
+        s)
+            USE_SHADERS="ON"
+            ;;
+        h)
+            echo "Usage: $0 [-r] [-s] [-h]"
+            echo ""
+            echo "Build script for vista"
+            echo ""
+            echo "Options:"
+            echo "  -r    Build in Release mode (default: Debug)"
+            echo "  -s    Enable shader support (default: OFF)"
+            echo "  -h    Show this help message"
+            exit 0
+            ;;
+        \?)
+            echo "Usage: $0 [-r] [-s] [-h]"
+            echo "Use -h for more information"
+            exit 1
+            ;;
+    esac
+done
 
 echo "Building vista..."
 echo "  Build type: $BUILD_TYPE"
@@ -23,8 +52,8 @@ cmake .. \
     -DUSE_SHADERS="$USE_SHADERS" \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
-# Build
-cmake --build . -j$(nproc)
+# Build using all available cores
+cmake --build . -j$(nproc --all)
 
 echo ""
 echo "Build complete! Binary: ./build/vista"
